26f1f3d03fcb5397d090b13920d7af62303e80bf
Ryan Chung <ryanchuung@gmail.com>
2020-11-25 06:57:09 -0800

    refactored components/About.js name, set up ServicesSection for About page

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import home1 from '../img/home1.png';
+
+
 const AboutSection = () => {
     return (
         <div>
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,15 @@
 import React from 'react';
 // Page Components
-import About from '../components/About';
+import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 
 const AboutUs = () => {
     return (
-        <About />
+        <>
+            <AboutSection />
+            <ServicesSection />
+        </>
     )
 }
 
