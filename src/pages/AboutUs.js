0f583e3ed4d1e9e2d44926bad70175c816ba76fe
Ryan Chung <ryanchuung@gmail.com>
2020-11-25 06:44:44 -0800

    about us section

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// Page Components
+import About from '../components/About';
+
+
+const AboutUs = () => {
+    return (
+        <About />
+    )
+}
+
+export default AboutUs;
