4066a48b9851287a10e1a7f1c950dd0cdc05323d
Ryan Chung <ryanchuung@gmail.com>
2020-11-25 10:20:20 -0800

    added BrowserRouter, Switch to pages, Link for nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+import { Switch, Route } from 'react-router-dom';
+
 import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
 import GlobalStyle from './components/GlobalStyle';
 import Nav from './components/Nav';
 
@@ -7,7 +11,17 @@ function App() {
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+        <Route path="/our-work">
+          <OurWork />
+        </Route>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
